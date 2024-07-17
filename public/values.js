02585793753620959d54a0f783f1f03db1b23e60
HokbikZdau <nguyenleminhtri05@gmail.com>
2024-07-17 09:54:09 +0700

    first commit

---
+++ public/values.js
@@ -0,0 +1,49 @@
+const inf = [
+    {
+        id:1,
+        title:"Music Player",
+        image:"./id_1.jpeg",
+    },
+    {
+        id:2,
+        title:"Console App",
+        image:"./id_2.png",
+    },
+    {
+        id:3,
+        title:"Baseball Infographic",
+        image:"./id_3.jpeg",
+    },
+    {
+        id:4,
+        title:"Powerwall App",
+        image:"./id_4.png",
+    },
+    {
+        id:5,
+        title:"Music App",
+        image:"./id_5.jpeg",
+    },
+    {
+        id:6,
+        title:"Photo App Icon",
+        image:"./id_6.png",
+    },
+    {
+        id:7,
+        title:"News Feed",
+        image:"./id_7.png",
+    },
+    {
+        id:8,
+        title:"Camera App",
+        image:"./id_8.jpeg",
+    },
+    {
+        id:9,
+        title:"Material exploration",
+        image:"./id_9.jpg",
+    }
+];
+
+export default {inf};
